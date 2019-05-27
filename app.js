d5cb1b0f3085c9b237aef5e43fec35d539d49536
Anette Andersson <anette.andersson.hem@gmail.com>
2019-05-27 16:42:32 +0200

    Adding template for app.js.

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World from node web server server_id_here!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
